4d2448ee42acc1b6ff8649a923f33f4fc7d1a013
Muhammad <happyness_hunter@yahoo.com>
2016-12-15 17:39:18 +0200

    JS File handle the click event
    In that JavaScript file we are handling the click event on the button and taking the path adding on it the query text that requested in the image tag field in the UI file.

---
+++ GooglePlaceAPI-Flask-Photos/static/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#search").click(function() {
+       var searchReq = $.get("/sendRequest/" + $("#query").val());
+        searchReq.done(function(data) {
+            $("#image").html(data);
+        });
+    });
+});
